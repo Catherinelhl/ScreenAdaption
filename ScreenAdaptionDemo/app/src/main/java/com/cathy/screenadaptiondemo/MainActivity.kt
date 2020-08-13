fd14c56ae456796573a2e15e3b0b7de7339573cf
刘红玲 <liuhongling@yungengxin.com>
2020-08-13 20:36:51 +0800

    默认就适配了？

---
+++ ScreenAdaptionDemo/app/src/main/java/com/cathy/screenadaptiondemo/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.cathy.screenadaptiondemo
+
+import com.cathy.screenadaptiondemo.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun getLayoutRes(): Int = R.layout.activity_main
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+
+    override fun initListener() {
+    }
+
+    override fun subscribeUI() {
+    }
+
+}
